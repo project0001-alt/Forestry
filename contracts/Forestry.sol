18488bbd5f7f5f1e6a8892aa74a2f4ca243996fe
project0001-alt <epitech.jordan@gmail.com>
2021-05-25 01:26:19 +0200

    license update

--- contracts/Forestry.sol
+++ contracts/Forestry.sol
@@ -1,9 +1,9 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract FORESTRY is ERC20 {
-    constructor(uint256 initialSupply) ERC20("FORESTRY", "FRST") {
-        _mint(msg.sender, initialSupply);
+contract Forestry is ERC20 {
+    constructor() ERC20("Forestry", "FRST") {
     }
 }
